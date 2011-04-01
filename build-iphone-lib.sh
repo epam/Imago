32c82e1476af1deceecf0401404c1b32d5929e9f
Victor Smolov <vsmolov@ggasoftware.com>
2011-04-01 18:09:31 +0400

    Script for iphone builds

---
+++ build-iphone-lib.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "" == "$1" ]
+then
+echo "Usage:"
+echo "	$0 <output-build-dir>"
+exit
+fi
+
+BUILD_DIR="$1"
+SOURCE_DIR=$(pwd)
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+/opt/local/bin/cmake -DIPHONE=ON -G Xcode $SOURCE_DIR
+cd -
