bf91227c5072e4530489803df417989d2b37672e
Rostislav Chutkov <rchutkov@ggasoftware.com>
2012-09-13 15:38:16 +0400

    updated font

--- tools/batches/font-update.bat
+++ tools/batches/font-update.bat
@@ -1,5 +1,6 @@
 cd "%~dp0\..\..\font_generator\"
-.\Release\font_generator.exe -D symbols -O ..\imago\src\imago.font
+.\Release\font_generator.exe -D symbols\base -O ..\imago\src\imago.font
+.\Release\font_generator.exe -D symbols\handwritten -O ..\imago\src\imago_handwritten.font
 cd ..
 CMake .
 pause
