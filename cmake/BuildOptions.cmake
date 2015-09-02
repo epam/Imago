a5185b188a997e3473a88c418cb743e50a37ccab
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2015-09-02 17:02:51 +0300

    imago-console: successfull build with third party libraries source code on Windows

--- cmake/BuildOptions.cmake
+++ cmake/BuildOptions.cmake
@@ -6,15 +6,15 @@ elseif(SYSTEM_NAME STREQUAL "Mac")
 	include(BuildOptionsMac)
 endif()
 
-set(LIBS_LOCATION "http://dp.itcwin.com/imago_dep" CACHE STRING "URL to directory with imago third party libs")
-if(NOT LIBS_LOCATION)
-	message(FATAL_ERROR "Please set LIBS_LOCATION variable")
-endif()
+#set(LIBS_LOCATION "http://dp.itcwin.com/imago_dep" CACHE STRING "URL to directory with imago third party libs")
+#if(NOT LIBS_LOCATION)
+#	message(FATAL_ERROR "Please set LIBS_LOCATION variable")
+#endif()
 set(THIRD_PARTY_DIR ${CMAKE_SOURCE_DIR}/third_party)
 
-include(Boost)
-include(OpenCV)
-include(Indigo)
+#include(Boost)
+#include(OpenCV)
+#include(Indigo)
 
 find_package(Java)
 
