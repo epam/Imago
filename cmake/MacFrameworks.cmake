01f0c416b44a5507133c343dcc311100f5f9c310
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2016-10-12 16:10:02 +0300

    imago-build: build fixed on macOS

---
+++ cmake/MacFrameworks.cmake
@@ -0,0 +1,38 @@
+# http://stackoverflow.com/questions/822404/how-to-set-up-cmake-to-build-an-app-for-the-iphone
+
+EXEC_PROGRAM(xcodebuild ARGS -version OUTPUT_VARIABLE XCODE_VERSION)
+string(REGEX MATCH "[0-9][.][0-9]" XCODE_VERSION ${XCODE_VERSION})
+
+if(UNIVERSAL_BUILD)
+    set(SSNAME 10.8)
+else()
+    set(SSNAME ${SUBSYSTEM_NAME})
+endif()
+
+if (${XCODE_VERSION} GREATER 4.2)
+    set(FRAMEWORK_PATH /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SSNAME}.sdk/System/Library/Frameworks)
+else()
+    set(FRAMEWORK_PATH /Developer/SDKs/MacOSX${SSNAME}.sdk/System/Library/Frameworks)
+endif()
+
+
+macro(ADD_FRAMEWORK fwname appname)
+    set(FRAMEWORK_${fwname} ${FRAMEWORK_PATH}/${fwname}.framework)
+
+    if(NOT IS_DIRECTORY ${FRAMEWORK_${fwname}})
+        message(ERROR ": Framework ${fwname} not found")
+    else()
+        target_link_libraries(${appname} ${FRAMEWORK_${fwname}})
+        message(STATUS "Framework ${fwname} found at ${FRAMEWORK_${fwname}}")
+    endif()
+endmacro()
+
+macro(FIND_FRAMEWORK fwname)
+    set(FRAMEWORK_${fwname} ${FRAMEWORK_PATH}/${fwname}.framework)
+
+    if(NOT IS_DIRECTORY ${FRAMEWORK_${fwname}})
+        message(ERROR ": Framework ${fwname} not found")
+    else()
+        message(STATUS "Framework ${fwname} found at ${FRAMEWORK_${fwname}}")
+    endif()
+endmacro()
--- imago/src/stl_fwd.h
+++ imago/src/stl_fwd.h
@@ -19,30 +19,46 @@
 #include "comdef.h"
 #include "vec2d.h"
 
-namespace std
-{
-   template<typename E> class allocator;
-   template<typename E, typename A> class vector;
-   template<typename E, typename A> class list;
-   template<typename E, typename A> class deque;
-   template<typename T1, typename T2> struct pair;
-}
+
+#include <deque>
+#include <list>
+#include <vector>
+#include <utility>
+
+// namespace std
+// {
+//    template<typename E> class allocator;
+//    template<typename E, typename A> class vector;
+//    template<typename E, typename A> class list;
+//    template<typename E, typename A> class deque;
+//    template<typename T1, typename T2> struct pair;
+// }
 
 namespace imago
 {
    class Segment;
 
-#define FWD_DECL(Container, Type, Name) typedef std::Container<Type, std::allocator<Type> > Name
-   FWD_DECL(list, int, IntList);
-   FWD_DECL(vector, int, IntVector);
-   FWD_DECL(vector, char, CharVector);
-   FWD_DECL(deque, int, IntDeque);
-   FWD_DECL(vector, double, DoubleVector);
-   FWD_DECL(list, Segment *, SegmentList);
-   FWD_DECL(deque, Segment *, SegmentDeque);
-   FWD_DECL(vector, Vec2d, Points2d);
-   FWD_DECL(vector, Vec2i, Points2i);
-   typedef std::pair<int, int> IntPair;
+typedef std::list<int, std::allocator<int> > IntList;
+typedef std::vector<int, std::allocator<int> > IntVector;
+typedef std::vector<char, std::allocator<char> > CharVector;
+typedef std::deque<int, std::allocator<int> > IntDeque;
+typedef std::vector<double, std::allocator<double> > DoubleVector;
+typedef std::list<Segment *, std::allocator<Segment *> > SegmentList;
+typedef std::deque<Segment *, std::allocator<Segment *> > SegmentDeque;
+typedef std::vector<Vec2d, std::allocator<Vec2d> > Points2d;
+typedef std::vector<Vec2i, std::allocator<Vec2i> > Points2i;
+typedef std::pair<int, int> IntPair;
+
+// #define FWD_DECL(Container, Type, Name) typedef std::Container<Type, std::allocator<Type> > Name
+//    FWD_DECL(list, int, IntList);
+//    FWD_DECL(vector, int, IntVector);
+//    FWD_DECL(vector, char, CharVector);
+//    FWD_DECL(deque, int, IntDeque);
+//    FWD_DECL(vector, double, DoubleVector);
+//    FWD_DECL(list, Segment *, SegmentList);
+//    FWD_DECL(deque, Segment *, SegmentDeque);
+//    FWD_DECL(vector, Vec2d, Points2d);
+//    FWD_DECL(vector, Vec2i, Points2i);
 }
 
 
