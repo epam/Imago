de992918f6de344aa6855dff1a4385884da08e3f
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2021-04-16 16:05:23 +0300

    build: parallelized build in build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 cd ..
 
 if [ -z "$1" ]; then
-  cmake --build build --config Release
+  cmake --build build --config Release -- -j $(nproc)
 else
-  cmake --build build --config Release --target $@
+  cmake --build build --config Release --target $@ -- -j $(nproc)
 fi
